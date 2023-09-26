a282c3b7f516f64dbff8056c9ccc5f743ba4fec9
maizaza <mai.siwach@gmail.com>
2023-09-26 22:05:34 +0700

    comment

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ const mongoose = require('mongoose')
 const expressSession = require('express-session')
 const flash = require('connect-flash')
 
-mongoose.connect('mongodb+srv://admin:0987654321z@cluster0.vhsadmo.mongodb.net/?retryWrites=true&w=majority', {
-    useNewUrlParser: true
-})
+// mongoose.connect('mongodb+srv://admin:0987654321z@cluster0.vhsadmo.mongodb.net/?retryWrites=true&w=majority', {
+//     useNewUrlParser: true
+// })
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
-app.use(express.static('public'))
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(flash())
-app.use(expressSession({secret: "node secret"}))
+// app.use(express.static('public'))
+// app.use(express.json())
+// app.use(express.urlencoded())
+// app.use(flash())
+// app.use(expressSession({secret: "node secret"}))
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 
