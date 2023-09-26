fd6f9f0a81f888332b3a699787e5eec9eed737e5
maizaza <mai.siwach@gmail.com>
2023-09-26 19:13:08 +0700

    change views

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const serverless = require('serverless-http')
 const app = express()
 const router = express.Router()
 const ejs = require('ejs');
+const path = require('path')
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
+app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
