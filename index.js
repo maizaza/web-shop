45d5664d649e02ed34f2203b5833ec822815ec88
maizaza <mai.siwach@gmail.com>
2023-09-27 09:44:40 +0700

    kuy2

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const express = require('express')
 const app = express()
 const ejs = require('ejs');
 const path = require('path')
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 const expressSession = require('express-session')
 const flash = require('connect-flash')
 
-mongoose
-  .connect('mongodb+srv://admin:0987654321z@cluster0.vhsadmo.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true })
-  .then(() => {
-    console.log('MongoDB connected');
-  })
-  .catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-  });
+// mongoose
+//   .connect('mongodb+srv://admin:0987654321z@cluster0.vhsadmo.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true })
+//   .then(() => {
+//     console.log('MongoDB connected');
+//   })
+//   .catch((err) => {
+//     console.error('Error connecting to MongoDB:', err);
+//   });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+// app.listen(3000, () => {
+//     console.log('Server is running on port 3000');
+// });
 
 // app.use(express.static('public'))
 // app.use(express.json())
