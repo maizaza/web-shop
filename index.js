05cc3a33b9ba1e65ba11f3935045f4a34acae239
maizaza <mai.siwach@gmail.com>
2023-09-27 10:21:20 +0700

    kuy3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const ejs = require('ejs');
 const path = require('path')
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 const expressSession = require('express-session')
 const flash = require('connect-flash')
 
@@ -19,11 +19,11 @@ const flash = require('connect-flash')
 //     console.log('Server is running on port 3000');
 // });
 
-// app.use(express.static('public'))
-// app.use(express.json())
-// app.use(express.urlencoded())
-// app.use(flash())
-// app.use(expressSession({secret: "node secret"}))
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(flash())
+app.use(expressSession({secret: "node secret"}))
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 
