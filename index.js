8eafeb64aa79839dba0419a18017ef36aa01efa2
maizaza <mai.siwach@gmail.com>
2023-09-27 10:28:37 +0700

    kuy4

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
+import * as mongoose from 'mongoose'
 const express = require('express')
 const app = express()
 const ejs = require('ejs');
 const path = require('path')
-const mongoose = require('mongoose')
 const expressSession = require('express-session')
 const flash = require('connect-flash')
 
-// mongoose
-//   .connect('mongodb+srv://admin:0987654321z@cluster0.vhsadmo.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true })
-//   .then(() => {
-//     console.log('MongoDB connected');
-//   })
-//   .catch((err) => {
-//     console.error('Error connecting to MongoDB:', err);
-//   });
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
 
-// app.listen(3000, () => {
-//     console.log('Server is running on port 3000');
-// });
+mongoose
+  .connect('mongodb+srv://admin:0987654321z@cluster0.vhsadmo.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true })
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
 app.use(express.static('public'))
 app.use(express.json())
